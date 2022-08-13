f7af7571c3e277db6d31ac51f20b5e4bc2ea968f
Fatima Salah <alanyfsalah@gmail.com>
2022-08-14 01:46:48 +0300

    'my last try Task1/dart'

--- BookClass.dart
+++ BookClass.dart
@@ -11,22 +11,23 @@ class Book {
 
   Book({this.name, this.author, this.price, this.rate});
 
-  //display AllBooks
+    //display AllBooks
   static void displayAllBooks() {
-    List<Book>? listBook = Book.booksList.cast<Book>();
-    if (listBook.isEmpty) {
-      print("There are no Books!");
-    } else {
-      print("Book Name\t\t\t\tBook Author\t\t\t\tPrice\t\t\t\tRate");
-      listBook.forEach((book) {
-        print(
-            "${book.getName}\t\t\t\t${book.getAuthor}\t\t\t\t${book.getPrice}\t\t\t\t${book.getRate}");
-      });
-    }
+    try{
+      List<Book>? listBook = Book.booksList.cast<Book>();
+      if (listBook.isEmpty) {
+        print("There are no Books!");
+      } else {
+        print("Book Name\t\t\t\tBook Author\t\t\t\tPrice\t\t\t\tRate");
+        listBook.forEach((book) {
+          print(
+              "${book.getName}\t\t\t\t${book.getAuthor}\t\t\t\t${book.getPrice}\t\t\t\t${book.getRate}");
+        });
+      }
+    }catch(e){print(e);}
   }
 
   //display Book with rate +4
-
   static void displayBook() {
     try{
       for (Book n in Book.booksList) {
@@ -49,55 +50,60 @@ class Book {
 
   //add function
   static void addBook(){
-    stdout.write('Enter the count of books want to add: ');
-    String? m =stdin.readLineSync();
+    try {
+      stdout.write('Enter the count of books want to add: ');
+      String? m = stdin.readLineSync();
+      if (m != '') {
+        for (int i = 1; i <= int.parse(m!); i++) {
+          stdout.write('Enter book $i name: ');
+          String? name = stdin.readLineSync();
+          stdout.write('Enter book $i author: ');
+          String? author = stdin.readLineSync();
+          stdout.write('Enter book $i price: ');
+          double? price = double.parse(stdin.readLineSync()!);
+          stdout.write('Enter book $i rate: ');
+          double? rate = double.parse(stdin.readLineSync()!);
 
-    for(int i = 1 ;i <=int.parse(m!);i++){
-      stdout.write('Enter book $i name: ');
-      String? name =stdin.readLineSync();
-      stdout.write('Enter book $i author: ');
-      String? author =stdin.readLineSync();
-      stdout.write('Enter book $i price: ');
-      double? price =double.parse(stdin.readLineSync()!);
-      stdout.write('Enter book $i rate: ');
-      double? rate =double.parse(stdin.readLineSync()!);
-
-      Book BOOKi=Book(name: name,author: author,price: price,rate: rate);
-      Book.booksList.add(BOOKi);
-    }
+          Book BOOKi = Book(
+              name: name, author: author, price: price, rate: rate);
+          Book.booksList.add(BOOKi);
+        }
+      }else{print('Nothing is entered,try again');}
+    }catch(e){print(e);}
   }
 
   //update function
-
   static void updateBook(){
-    try{
+    try {
       stdout.write('Enter the book name: ');
-      String? name =stdin.readLineSync();
-      for (Book n in Book.booksList) {
-        if (n.getName! == name) {
-          stdout.write('Enter book new name: ');
-          String? newName =stdin.readLineSync();
-          stdout.write('Enter book new author: ');
-          String? newAuthor =stdin.readLineSync();
-          stdout.write('Enter book new price: ');
-          double? newPrice =double.parse(stdin.readLineSync()!);
-          stdout.write('Enter book new rate: ');
-          double? newRate =double.parse(stdin.readLineSync()!);
-          n.setName=newName;
-          n.setAuthor=newAuthor;
-          n.setPrice=newPrice;
-          n.setRate=newRate;
+      String? name = stdin.readLineSync();
+      if (name != '') {
+        for (Book n in Book.booksList) {
+          if (n.getName! == name) {
+            stdout.write('Enter book new name: ');
+            String? newName = stdin.readLineSync();
+            stdout.write('Enter book new author: ');
+            String? newAuthor = stdin.readLineSync();
+            stdout.write('Enter book new price: ');
+            double? newPrice = double.parse(stdin.readLineSync()!);
+            stdout.write('Enter book new rate: ');
+            double? newRate = double.parse(stdin.readLineSync()!);
+            n.setName = newName;
+            n.setAuthor = newAuthor;
+            n.setPrice = newPrice;
+            n.setRate = newRate;
+          }
         }
-      }
+      }else{ print('Nothing is entered,try again');}
     }catch(e){print(e);}
   }
 
   //delete function
-
   static void deleteBook() {
     try{
         stdout.write('Enter the book name: ');
         String? name = stdin.readLineSync();
+        if(name != ''){
         for (Book n in booksList){
           if (n.getName == name) {
             booksList.remove(n);
@@ -105,41 +111,46 @@ class Book {
       }else{
             print('There are no Books!');
             break;
-          }}
+          }
+        }}else{
+          print('Nothing is entered,try again');
+        }
   }catch(e){
     print(e);}
   }
 
   //search function
-
   static void searchBook(){
-    try{
+    try {
       stdout.write('Enter the book name: ');
-      String? name =stdin.readLineSync();
-      for (Book n in Book.booksList) {
-        if (n.getName!.contains(name!)) {
-          Book.bookList.add(n);
+      String? name = stdin.readLineSync();
+      if (name != '') {
+        for (Book n in Book.booksList) {
+          if (n.getName!.contains(name!)) {
+            Book.bookList.add(n);
+          }
         }
-      }
-      if (bookList.isEmpty) {
-        print("There are no Books!");
-      } else {
-        print("Name\t\t\t\tAuthor\t\t\t\tPrice\t\t\t\tRate");
-        for (Book n in bookList) {
-          print(
-              "${n.getName}\t\t\t\t${n.getAuthor}\t\t\t\t${n.getPrice}\t\t\t\t${n.getRate}");
+        if (bookList.isEmpty) {
+          print("There are no Books!");
+        } else {
+          print("Name\t\t\t\tAuthor\t\t\t\tPrice\t\t\t\tRate");
+          for (Book n in bookList) {
+            print(
+                "${n.getName}\t\t\t\t${n.getAuthor}\t\t\t\t${n
+                    .getPrice}\t\t\t\t${n.getRate}");
+          }
         }
-      }
-      Book.bookList.clear();
+        Book.bookList.clear();
+      }else{print('Nothing is entered,try again');}
     }catch(e){print(e);}
   }
 
-  //Here is getters
-  String? get getName => this.name;
-  String? get getAuthor => this.author;
+  //Here are getters
+  String? get getName => this.name?.toUpperCase();
+  String? get getAuthor => this.author?.toUpperCase();
   double? get getPrice => this.price;
   double? get getRate => this.rate;
-  //Here is setters
+  //Here are setters
   set setName(String? newName) => name = newName;
   set setAuthor(String? newAuthor)=> author=newAuthor;
   set setPrice(double? newPrice)=> price=newPrice ;
